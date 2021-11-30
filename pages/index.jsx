d3a867440e10fdd85a48f835fcc89780759ac3ef
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-30 20:11:11 -0300

    Inicial Commit

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-auto mb-8 ">
+      <Head>
+        <title>Toppo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+
+      </div>
+    </div>
+  );
+}
